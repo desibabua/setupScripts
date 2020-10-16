170397504891b184b6564ee361ddb879e4dcfae3
ayush <58028488+desibabua@users.noreply.github.com>
2020-10-16 19:32:11 +0530

    updated set up script

--- setUp.sh
+++ setUp.sh
@@ -3,4 +3,9 @@
 brew install npm
 brew install npx
 npm intall tree
+
 npm install git
+
+git config --global user.name "desibabua"
+git config --global user.email "kumarayush074@gmail.com"
+git config --global pull.rebase true
