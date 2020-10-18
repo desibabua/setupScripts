1433c511eab0c9882c1d4d2fd01b482c3e850061
ayush <58028488+desibabua@users.noreply.github.com>
2020-10-18 21:29:25 +0530

    added autosuggestion and highlighting scripts in setup.sh

--- set_up.sh
+++ set_up.sh
@@ -2,6 +2,7 @@
 
 brew install npm
 brew install npx
+brew install bat
 npm intall tree
 
 npm install git
@@ -13,3 +14,10 @@ git config --global alias.unstage rebase
 
 git config --global filter.tabspace.smudge 'unexpand --tabs=2 --first-only'
 git config --global filter.tabspace.clean 'expand --tabs=2 --initial'
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# restart
+p10k configure
