8240b9e88e0c46a41eeb46cf5291d0069f1bd870
Ayush kumar <58028488+desibabua@users.noreply.github.com>
2021-03-01 18:39:09 +0530

    added githooks path

--- set_up.sh
+++ set_up.sh
@@ -11,6 +11,7 @@ git config --global user.name "Ayush"
 git config --global user.email "ayush.kumar@thoughtworks.com"
 git config --global pull.rebase true
 git config --global alias.unstage rebase
+git config --global core.hooksPath .githooks/
 
 git config --global filter.tabspace.smudge 'unexpand --tabs=2 --first-only'
 git config --global filter.tabspace.clean 'expand --tabs=2 --initial'
